8fb74cf6baf75d386f76d04c260022ed65496e5a
evgeniy-nikolaev <64286497+evgeniy-nikolaev@users.noreply.github.com>
2022-02-16 19:13:51 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+defined('VG_ACCESS') or die('Access denied');
+const SITE_URL = 'http://onshop.my';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'onshop';
+
