81693c9699dccaa2e7ad2f094263ca3dcb7bd1d0
evgeniy-nikolaev <evgeniy793504@gmail.com>
2022-02-19 18:35:31 +0300

    change settings.php, add print_arr

--- core/base/controllers/RouteController.php
+++ core/base/controllers/RouteController.php
@@ -1,30 +1,23 @@
 <?php
-
 namespace core\base\controllers;
-
 use core\base\settings\Settings;
-
 class RouteController
 {
-
     static private $_instance;
-
     private function __clone()
     {
     }
-
     static public function getInstance(){
         if(self::$_instance instanceof self){
             return self::$_instance;
         }
-
         return self::$_instance = new self;
     }
-
     private function __construct()
     {
+        $arr = ['1', 2, 3, 4, 5];
+        print_arr($arr);
         $s = Settings::get('routes');
         exit();
     }
-
 }
